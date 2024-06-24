5b1537a81654d59f9196aa43f23a28ba271c1562
Paulsalinas1 <paul.salinasc@duocuc.cl>
2024-06-24 18:21:20 -0400

    a

--- mi_web/models.py
+++ mi_web/models.py
@@ -49,5 +49,7 @@ class Tarjeta(models.Model):
     tarjeta_de_credito = models.IntegerField("numero tarjeta", unique=True)
     fecha_de_vencimiento = models.CharField("fecha vencimiento", max_length=50)
     codigo_de_seguridad = models.IntegerField("cv")
+    
+
 
     
